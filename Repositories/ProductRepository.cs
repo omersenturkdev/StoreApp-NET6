71c0a152feebfcbfe8c1176942c3aae136d76fa6
Omer Senturk <omersenturk.dev@gmail.com>
2024-08-17 18:11:04 +0300

    repositorybase created

---
+++ Repositories/ProductRepository.cs
@@ -0,0 +1,21 @@
+﻿using Entities.Models;
+using Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories
+{
+    public class ProductRepository : RepositoryBase<Product>, IProductRepository
+    {
+        public ProductRepository(RepositoryContext context) : base(context)
+        {
+
+        }
+
+        public IQueryable<Product> GetAllProduct(bool trackChanged) => FindAll(trackChanged);
+       
+    }
+}
