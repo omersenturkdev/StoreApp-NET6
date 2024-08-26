0b5d69189d741ef1ac6b8f67b9148b736eb02745
Omer Senturk <omersenturk.dev@gmail.com>
2024-08-27 02:26:15 +0300

    category entity created

--- Repositories/RepositoryContext.cs
+++ Repositories/RepositoryContext.cs
@@ -6,6 +6,8 @@ namespace Repositories
     public class RepositoryContext : DbContext
     {
         public DbSet<Product> Products { get; set; }
+
+        public DbSet<Category> Categories { get; set; }
         public RepositoryContext(DbContextOptions<RepositoryContext> options) : base(options)
         {
 
@@ -34,6 +36,19 @@ namespace Repositories
                         Price = 8000
                     }
                 );
+            modelBuilder.Entity<Category>()
+               .HasData(
+                   new Category()
+                   {
+                       CategoryId = 1,
+                       CategoryName = "Book"
+                   },
+                   new Category()
+                   {
+                       CategoryId = 2,
+                       CategoryName = "Electronic"
+                   }
+               );
         }
 
     }
