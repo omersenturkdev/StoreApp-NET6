7cdc7dd486ba7674d17cd0846a94eb8b8493c086
Omer Senturk <omersenturk.dev@gmail.com>
2024-08-28 18:11:35 +0300

    ViewComponent added project

---
+++ StoreApp/Components/ProductSummaryViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Repositories;
+using Services.Contracts;
+
+namespace StoreApp.Components
+{
+    public class ProductSummaryViewComponent : ViewComponent
+    {
+       
+        private readonly IServiceManager _serviceManager;
+
+        public ProductSummaryViewComponent(IServiceManager serviceManager)
+        {
+            _serviceManager = serviceManager;
+        }
+
+        //Invoke adı verilmesi zorunlu.
+        public string Invoke()
+        {
+            return _serviceManager.ProductService.GetProducts(false).Count().ToString();
+        }
+
+    }
+}
